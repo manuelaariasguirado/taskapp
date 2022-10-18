957e453684196a4082bb3b6bb788dfc9d8c0e9bc
Manuela <113796404+manuelaariasguirado@users.noreply.github.com>
2022-10-18 12:52:58 +0200

    build

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,21 @@
 <template>
   <h1>task app</h1>
+  <ul>
+    <li>
+      <router-link :to="{name: 'home'}">Home</router-link>
+    </li>
+    <li>
+      <router-link :to="{name: 'login'}">Login</router-link>
+    </li>
+  </ul>
+  <router-view></router-view>
 </template>
 <script setup>
-console.log(process.env.NODE_ENV)
+import { createClient } from '@supabase/supabase-js'
+
+const supabase = createClient(import.meta.VITE_SUPABASE_URL, import.meta.VITE_SUPABASE_KEY)
+console.log(supabase)
+
 </script>
 <style scoped>
 </style>
